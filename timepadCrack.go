f8c426193f2232f385bfe03f556a993e08c7d84c
Jesús Gazol <jgazol@funidelia.es>
2015-07-15 13:38:18 +0200

    Done with onetimepad

--- timepadCrack.go
+++ timepadCrack.go
@@ -10,6 +10,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -18,14 +19,37 @@ func main() {
 	byteText := readFile("cipheredTextTimePad.txt")
 	fmt.Println(byteText)
 	var i byte
+	solution := []byte{242, 26, 4, 155, 208, 115, 35, 200, 57, 152,
+		206, 9, 14, 188, 134, 218, 201, 224, 57, 137,
+		42, 95, 114, 103, 131, 165, 97, 253, 37, 238,
+		48}
+
+	checkSolution(byteText, solution)
+	byteText = readFile("cipheredTextTimePad.txt")
+	// Look for spaces
+	//findSpaceColumns(byteText)
+
+	// Look for remaining
 	for i = 1; i < 255; i++ {
 		for column := 0; column < len(byteText[1]); column++ {
 			if checkTransform(byteText, column, i) == true {
-				fmt.Printf("\nByte %d makes sense for column %d ", i, column)
+				fmt.Printf(" byte %d makes sense for column %d \n", i, column)
 			} else {
 				//fmt.Printf("Bad %d %d", i, column)
 			}
 		}
+	}
+}
+
+func checkSolution(text [][]byte, solution []byte) {
+
+	for col := 0; col < len(solution); col++ {
+		for row := 0; row < 7; row++ {
+			text[row][col] = text[row][col] ^ solution[col]
+		}
+	}
+	for row := 0; row < 7; row++ {
+		fmt.Println(string(text[row]))
 	}
 }
 
@@ -43,14 +67,25 @@ func checkTransform(byteText [][]byte, column int, index byte) bool {
 }
 
 func hasGoodChars(text []byte) bool {
+	result := true
+	//alphaChars := 0
+	re := regexp.MustCompile(`[[:lower:]]|[[:blank:]]|,|\.|;|\!|\?`)
 	for i := 0; i < len(text); i++ {
 		if text[i] < 32 || text[i] > 122 {
-			return false
+			result = false
 		}
 	}
 
-	//Check counters
-	return true
+	if result == true {
+		//fmt.Print("\n Found strings")
+		//fmt.Print(re.FindAllStringIndex(string(text), -1))
+		//fmt.Printf(" - %s\n", string(text))
+		if len(re.FindAllStringIndex(string(text), -1)) < 7 {
+			result = false
+		}
+
+	}
+	return result
 }
 
 func readFile(fileName string) [][]byte {
